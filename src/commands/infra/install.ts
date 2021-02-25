9bea791cd3ba3babd203533ba1c5d1a4adf4f776
Carolina Menezes <carolinamenezes@hotmail.com.br>
2021-02-25 16:06:17 -0300

    Update description

--- src/commands/infra/install.ts
+++ src/commands/infra/install.ts
@@ -1,14 +1,25 @@
-import { CustomCommand } from 'vtex'
+import { CustomCommand, ColorifyConstants } from 'vtex'
 import appsInfraInstall from '../../modules/install'
 
 export default class InfraInstall extends CustomCommand {
-  static description = 'Install an infra service'
+  static description = 'Installs an infra service.'
 
-  static examples = ['vtex infra install infra-service', 'vtex infra install infra-service@0.0.1']
+  static examples = [
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex infra install')} infra-service`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex infra install')} infra-service@0.0.1`,
+  ]
 
   static flags = { ...CustomCommand.globalFlags }
 
-  static args = [{ name: 'serviceId', required: true }]
+  static args = [
+    {
+      name: 'serviceId',
+      required: true,
+      description: `Name and version of the service ${ColorifyConstants.ID(
+        '({vendor}.{servicename}@{x.x.x})'
+      )} to install.`,
+    },
+  ]
 
   async run() {
     const { args } = this.parse(InfraInstall)
